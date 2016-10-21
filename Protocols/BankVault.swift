b983413a2a55481c1e088fd7eabfe38f9ce01216
mariejinpark <mariejinpark@gmail.com>
2016-10-21 06:18:00 -0400

    Banks and Ships

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -17,10 +17,31 @@ class BankVault {
         self.name = name
         self.address = address
     }
-    
 }
 
+protocol ProvideAccess {
+    func allowEntryWithPassword (password: [Int]) -> Bool
+}
 
-
-
+extension BankVault: ProvideAccess {
+    func allowEntryWithPassword(password: [Int]) -> Bool {
+        var incorrectPassword = false
+        
+        for i in password {
+            if password.isEmpty {
+                return false
+            } else if  (password.count) > 10 {
+                return false
+            } else {
+                return true
+            }
+//                for (index, value) in password.enumerated() {
+//                    if index % 2 == 0 && value % 2 == 0 {
+//                        return true
+//                    }
+//                }
+        }
+    return incorrectPassword
+    }
+}
 
